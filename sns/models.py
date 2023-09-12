dc8bca8cdc30a1f6b06f0ac2eeb2ce4e29909613
bainaryho <rkswl233@naver.com>
2023-09-13 03:24:45 +0900

    sns) makemigrations

--- sns/models.py
+++ sns/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # User 모델 외래키
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:8]  # 게시글 내용 8글자까지만 반환
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="follower"
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"  # 팔로워 팔로잉 관계 문자열로 반환

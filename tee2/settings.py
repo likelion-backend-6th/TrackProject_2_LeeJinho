0418e996d95800020464d817d46fe1739758583e
bainaryho <rkswl233@naver.com>
2023-09-19 04:00:05 +0900

    static with eginx

--- tee2/settings.py
+++ tee2/settings.py
@@ -135,15 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# 기존 설정
-STATIC_URL = "/static/"
-# 추가 설정
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static", "static"),
-    "/app/static",
-]
+STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = "/var/www/html/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

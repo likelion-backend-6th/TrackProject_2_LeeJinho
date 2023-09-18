1ba38ccd0dd8e5523691832e16642e56881c5031
bainaryho <rkswl233@naver.com>
2023-09-19 00:42:54 +0900

    정적파일 추가

--- tee2/settings.py
+++ tee2/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = "/var/www/html/static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

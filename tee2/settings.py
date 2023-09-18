574513c18b49f7ee054d5113e15bd6c1d3d24f5b
bainaryho <rkswl233@naver.com>
2023-09-19 03:15:53 +0900

    정적파일수집 재시도

--- tee2/settings.py
+++ tee2/settings.py
@@ -135,14 +135,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
+
 STATIC_ROOT = "/var/www/html/static"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
 
 AUTH_USER_MODEL = "auth.User"
 
